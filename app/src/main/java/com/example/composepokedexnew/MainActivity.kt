426bdbde78b502e137c87463cfcf372d7d2889de
martsilva <martin.silva@mercadolibre.com>
2025-05-27 13:13:03 -0300

    test

--- app/src/main/java/com/example/composepokedexnew/MainActivity.kt
+++ app/src/main/java/com/example/composepokedexnew/MainActivity.kt
@@ -17,6 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        // asd
         setContent {
             JetpackComposePokedexTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
