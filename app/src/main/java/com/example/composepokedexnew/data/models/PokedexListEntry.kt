9f346b952867feb2b3f0a0c8fd5653806a6d9571
Martin-A-Silva <a.i.silvamartin@gmail.com>
2025-05-28 15:13:00 -0300

    Added PokedexEntry and PokedexRow

---
+++ app/src/main/java/com/example/composepokedexnew/data/models/PokedexListEntry.kt
@@ -0,0 +1,8 @@
+package com.example.composepokedexnew.data.models
+
+data class PokedexListEntry(
+    val pokemonName: String,
+    val imageUrl: String,
+    val number: Int,
+    val types: List<String>
+)
--- app/src/main/java/com/example/composepokedexnew/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/example/composepokedexnew/pokemonlist/PokemonListScreen.kt
@@ -2,15 +2,22 @@ package com.example.composepokedexnew.pokemonlist
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -21,14 +28,27 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import coil.compose.SubcomposeAsyncImage
+import coil.request.ImageRequest
 import com.example.composepokedexnew.R
+import com.example.composepokedexnew.data.models.PokedexListEntry
+import com.example.composepokedexnew.ui.theme.RobotoCondensed
+import java.util.Map.entry
 
 @Composable
 fun PokemonListScreen(navController: NavController, modifier: Modifier = Modifier) {
@@ -90,6 +110,114 @@ fun SearchBar(
                 color = Color.LightGray,
                 modifier = Modifier.padding(horizontal = 20.dp, vertical = 12.dp)
             )
+        }
+    }
+}
+
+@Composable
+fun PokedexEntry(
+    entry: PokedexListEntry,
+    navController: NavController,
+    modifier: Modifier = Modifier,
+    viewModel: PokemonListViewModel = hiltViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colors.background
+    var dominantColor by remember { mutableStateOf(defaultDominantColor) }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+            .shadow(5.dp, RoundedCornerShape(10.dp))
+            .clip(RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(
+                Brush.verticalGradient(
+                    listOf(
+                        dominantColor,
+                        defaultDominantColor
+                    )
+                )
+            )
+            .clickable {
+                navController.navigate("pokemon_detail_screen/$dominantColor/${entry.pokemonName}")
+            }
+    ) {
+        Column {
+            AsyncImage(
+                model = ImageRequest.Builder(
+                    LocalContext.current
+                )
+                    .data(entry.imageUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = entry.pokemonName,
+                onSuccess = {
+                    viewModel.calcDominantColor(it.result.drawable) { color ->
+                        dominantColor = color
+                    }
+                },
+                modifier = modifier
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            SubcomposeAsyncImage(
+                model = ImageRequest.Builder(
+                    LocalContext.current
+                )
+                    .data(entry.imageUrl)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = entry.pokemonName,
+                onSuccess = {
+                    viewModel.calcDominantColor(it.result.drawable) { color ->
+                        dominantColor = color
+                    }
+                },
+                modifier = modifier
+                    .size(120.dp)
+                    .align(Alignment.CenterHorizontally),
+                loading = {
+                    CircularProgressIndicator(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier
+                            .scale(0.5f)
+                    )
+                }
+            )
+            Text(
+                text = entry.pokemonName,
+                fontFamily = RobotoCondensed,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Composable
+fun PokedexRow(
+    rowIndex: Int,
+    entries: List<PokedexListEntry>,
+    navController: NavController
+) {
+    Column {
+        Row {
+            PokedexEntry(
+                entry = entries[2 * rowIndex],
+                navController = navController,
+                modifier = Modifier.weight(1f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            if (entries.size >= 2 * rowIndex + 2) {
+                PokedexEntry(
+                    entry = entries[2 * rowIndex + 1],
+                    navController = navController,
+                    modifier = Modifier.weight(1f)
+                )
+            } else {
+                Spacer(modifier = Modifier.width(16.dp))
+            }
         }
     }
 }
